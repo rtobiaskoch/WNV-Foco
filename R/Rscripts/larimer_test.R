58a0f505b66e2e4f99564612ab399587a700e82e
Robert Koch <r.tobiaskoch@gmail.com>
2023-09-19 12:26:40 -0600

    before removing larimer from mdata_other

---
+++ R/Rscripts/larimer_test.R
@@ -0,0 +1,21 @@
+rm(list = ls())
+pacman::p_load(tidyverse, Biostrings, readr)
+source("https://raw.githubusercontent.com/rtobiaskoch/TK_useful_functions/main/fasta_filter.R")
+
+
+mdata = read_rds("data_output/mdata_c_cmplt.rds")
+
+fasta = Biostrings::readDNAStringSet("data_output/fasta_ns.fasta")
+
+larimer = mdata %>%
+  filter(grepl("Larimer", division) & grepl("Larimer", trap)) %>%
+  sample_n(3)
+
+csu = mdata %>%
+  filter(date  %in% larimer$date)
+
+t = rbind(csu, larimer)         
+
+f = filter_fasta(fasta, t$strain)
+
+Biostrings::writeXStringSet(f, "data_mid/larimer_fasta_test.fasta")
